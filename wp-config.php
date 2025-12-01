5e6480f7bd9857c4970adda4609102bffe86e6bd
Andres Cruz  Ortiz <andres_brother@outlook.com>
2025-11-30 23:48:33 -0600

    Revert "Tienda de vinilos: Eliminados plugins CartFlows y WCAR, agregado router.php personalizado"
    This reverts commit 47abe4c6d82e7fff7d4d6d95da1e9d96de959f15.

--- wp-config.php
+++ wp-config.php
@@ -79,9 +79,7 @@ $table_prefix = 'f1bd_';
  *
  * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_DISPLAY', true );
-define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
